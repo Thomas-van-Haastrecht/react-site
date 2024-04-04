bdd12e6d67344d016ed2838a3d13e8c9edae1ddf
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-04-04 05:06:05 -0400

    added PUT function for recipes

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -4,3 +4,12 @@ import {fetchData} from './fetch'
 export function getRecipes () {
     return fetchData('https://localhost:7027/api/recipes');
 }
+
+// function to update (PUT) an existing recipe
+export function putRecipe ({id, recipeJSON}) { // destructoring necessary as react-query mutate functions only take one input
+    return fetch('https://localhost:7027/api/recipes/'+id, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: recipeJSON,
+    });
+}
