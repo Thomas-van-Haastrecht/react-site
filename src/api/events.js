45c9b9ab9d770bc661782b08239d225648454edd
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-04-09 06:41:42 -0400

    added functions to DELETE events and participants

--- src/api/events.js
+++ src/api/events.js
@@ -1,11 +1,11 @@
 import {fetchData} from './fetch'
 
-// function to GET allergy info from DB
+// function to GET event info from DB
 export function getEvents () {
     return fetchData('https://localhost:7027/api/events');
 }
 
-// function to POST a new product to DB
+// function to POST a new event to DB
 export function postEvent (eventJSON) {
     return fetch('https://localhost:7027/api/events', {
         method: 'POST',
@@ -14,11 +14,28 @@ export function postEvent (eventJSON) {
     });
 }
 
-// function to update (PUT) an existing product
+// function to update (PUT) an existing event
 export function putEvent ({id, eventJSON}) { // destructoring necessary as react-query mutate functions only take one input
     return fetch('https://localhost:7027/api/events/'+id, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: eventJSON,
     });
+}
+
+// function to DELETE an event
+export function deleteEvent (id) {
+    return fetch('https://localhost:7027/api/events/'+id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+    })
+}
+
+// function to DELETE a participant from an event
+export function deleteParticipant ({id, participantJSON}) {
+    return fetch('https://localhost:7027/api/events/'+id+'/participants', {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: participantJSON,
+    })
 }
